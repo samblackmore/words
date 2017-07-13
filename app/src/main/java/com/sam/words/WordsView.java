e33790dd05ccb14c1f3e85df26556a8c173545b7
Sam Blackmore <samblackmore07@gmail.com>
2017-07-12 20:14:30 -0400

    adding and measuring font

--- app/src/main/java/com/sam/words/WordsView.java
+++ app/src/main/java/com/sam/words/WordsView.java
@@ -1,19 +1,24 @@
 package com.sam.words;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WordsView extends View {
 
     private int firstLetterWidth;
-    private int firstLetterHeight;
+    private int sentenceHeight;
     private Paint mTextPaint;
-    private String firstLetter = "A";
+    private String sentence = "AalqQ";
+    private List<Rect> sentenceBounds;
     private int paddingHor = 100;
     private int paddingVer = 200;
     private int viewLeft = -1;
@@ -21,16 +26,34 @@ public class WordsView extends View {
     private int viewTop = -1;
     private int viewBottom = -1;
 
+    private List<Rect> getBoundsPerLetter(Paint paint, String string) {
+        List<Rect> list = new ArrayList<>();
+        for (int i = 0; i < string.length(); i++) {
+            Rect bounds = new Rect();
+            paint.getTextBounds(string, i, i + 1, bounds);
+            float width = paint.measureText(string, i, i + 1);
+            bounds.set(0, 0, (int) width, bounds.height());
+            list.add(bounds);
+        }
+        return list;
+    }
+
 
     public WordsView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        Typeface crimsonText = Typeface.createFromAsset(
+                context.getAssets(),
+                "fonts/CrimsonText/CrimsonText-Regular.ttf"
+        );
         mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setTypeface(crimsonText);
         mTextPaint.setTextSize(300);
         mTextPaint.setStyle(Paint.Style.STROKE);
+        sentenceBounds = getBoundsPerLetter(mTextPaint, sentence);
         Rect bounds = new Rect();
-        mTextPaint.getTextBounds(firstLetter, 0, 1, bounds);
-        firstLetterWidth = bounds.width();
-        firstLetterHeight = bounds.height();
+        mTextPaint.getTextBounds(sentence, 0, sentence.length()-1, bounds);
+        sentenceHeight = bounds.height();
+
     }
 
     @Override
@@ -50,19 +73,24 @@ public class WordsView extends View {
             viewBottom  = getBottom();
 
         int textX = viewLeft + paddingHor;
-        int textY = viewTop + paddingVer;
+        int textY = viewTop + paddingVer + 200;
 
         canvas.drawText(
-                firstLetter,
-                textX,
-                textY + firstLetterHeight,
-                mTextPaint);
-        canvas.drawRect(
+                sentence,
                 textX,
                 textY,
-                textX + firstLetterWidth,
-                textY + firstLetterHeight,
                 mTextPaint);
+
+        int nextX = textX;
+        for (Rect rect : sentenceBounds) {
+            canvas.drawRect(
+                    nextX,
+                    textY,
+                    nextX + rect.width(),
+                    textY - rect.height(),
+                    mTextPaint);
+            nextX += rect.width();
+        }
 
         canvas.drawRect(
                 viewLeft + 5,
