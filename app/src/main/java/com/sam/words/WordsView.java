1a7d67582dbe506939b4a204516cb184d6654189
Sam Blackmore <samblackmore07@gmail.com>
2017-07-13 20:15:22 -0400

    calculating drop cap height

--- app/src/main/java/com/sam/words/WordsView.java
+++ app/src/main/java/com/sam/words/WordsView.java
@@ -22,7 +22,7 @@ public class WordsView extends View {
     private List<Rect> sentenceBounds;
     private int paddingHor = 100;
     private int paddingVer = 200;
-    private int textSizePx = 10;
+    private int textSizePx = 100;
     private int viewLeft = -1;
     private int viewRight = -1;
     private int viewTop = -1;
@@ -34,6 +34,8 @@ public class WordsView extends View {
     private float lineSeperation = 1.5f;
     private int lineSpacing;
     private int lineHeight;
+    private int dropCapHeight;
+    private int dropCapTextSize;
 
     private void calculateDimensions() {
         if (viewLeft == -1) {
@@ -88,6 +90,39 @@ public class WordsView extends View {
         return list;
     }
 
+    private int getFontSizeToMatchLineHeight(Paint paint, int targetLineHeight) {
+        float savedTextSize = paint.getTextSize();
+        int newSize = 0;
+        int stepSize = 100;
+        int lineHeight = 0;
+        boolean goingUp = true;
+        Rect bounds = new Rect();
+
+        while (lineHeight != targetLineHeight) {
+
+            paint.setTextSize(newSize);
+            paint.getTextBounds("A", 0, 1, bounds);
+            lineHeight = bounds.height();
+
+            if (lineHeight < targetLineHeight) {
+                if (!goingUp) {
+                    goingUp = true;
+                    stepSize /= 2;
+                }
+                newSize += stepSize;
+            }
+            if (lineHeight > targetLineHeight) {
+                if (goingUp) {
+                    goingUp = false;
+                    stepSize /= 2;
+                }
+                newSize -= stepSize;
+            }
+        }
+        paint.setTextSize(savedTextSize);
+        return newSize;
+    }
+
     public void increaseTextSize(int amount) {
         textSizePx += amount;
         init();
@@ -101,6 +136,9 @@ public class WordsView extends View {
         mTextPaint.getTextBounds("l", 0, 1, bounds);
         lineHeight = bounds.height();
         lineSpacing = (int) ((lineHeight * lineSeperation) - lineHeight);
+
+        dropCapHeight = (3 * lineHeight) + (2 * lineSpacing);
+        dropCapTextSize = getFontSizeToMatchLineHeight(mTextPaint, dropCapHeight);
 
         //sentenceBounds = getBoundsPerLetter(mTextPaint, sentence);
     }
@@ -133,6 +171,15 @@ public class WordsView extends View {
                     textAreaTop + (i*lineHeight) + ((i-1) * lineSpacing),
                     mTextPaint);
         }
+
+        float savedTextHeight = mTextPaint.getTextSize();
+        mTextPaint.setTextSize(dropCapTextSize);
+        canvas.drawText(
+                "A",
+                textAreaLeft,
+                textAreaTop + dropCapHeight,
+                mTextPaint);
+        mTextPaint.setTextSize(savedTextHeight);
 
         /*int nextX = textAreaLeft;
         for (Rect rect : sentenceBounds) {
