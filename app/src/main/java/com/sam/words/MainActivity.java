7b0ff3ef64a1fb11193e24ac465f9cd27af4a44a
Sam Blackmore <samblackmore07@gmail.com>
2017-07-17 23:42:47 -0400

    styling

--- app/src/main/java/com/sam/words/MainActivity.java
+++ app/src/main/java/com/sam/words/MainActivity.java
@@ -1,21 +1,35 @@
 package com.sam.words;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     WordsView wordsView;
 
+    private void setupButton(int buttonId, int colorId, int textColorId) {
+        Button button = (Button) findViewById(buttonId);
+        int color = getResources().getColor(colorId);
+        int textColor = getResources().getColor(textColorId);
+        button.getBackground().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
+        button.setTextColor(textColor);
+        button.setOnClickListener(this);
+        button.invalidate();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btn_increase_font_size).setOnClickListener(this);
-        findViewById(R.id.btn_decrease_font_size).setOnClickListener(this);
-        findViewById(R.id.btn_start).setOnClickListener(this);
+        setupButton(R.id.btn_increase_font_size, R.color.white, R.color.black);
+        setupButton(R.id.btn_decrease_font_size, R.color.white, R.color.black);
+        setupButton(R.id.btn_start, R.color.colorAccent, R.color.white);
+
         wordsView = (WordsView) findViewById(R.id.words_view);
     }
 
