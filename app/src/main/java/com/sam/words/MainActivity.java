ec2a4358770d89c2f9ee427ee4561de7e8b91ff0
Sam Blackmore <samblackmore07@gmail.com>
2017-07-17 22:11:03 -0400

    adding swipe and tab activities

--- app/src/main/java/com/sam/words/MainActivity.java
+++ app/src/main/java/com/sam/words/MainActivity.java
@@ -1,26 +1,34 @@
 package com.sam.words;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    WordsView wordsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_increase_font_size).setOnClickListener(this);
+        findViewById(R.id.btn_start).setOnClickListener(this);
+        wordsView = (WordsView) findViewById(R.id.words_view);
+    }
 
-        final WordsView wordsView = (WordsView) findViewById(R.id.words_view);
-        View embiggen = findViewById(R.id.btn_increase_font_size);
-
-        embiggen.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_start:
+                Intent intent = new Intent(this, TabActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_increase_font_size:
                 wordsView.increaseTextSize(10);
                 wordsView.invalidate();
-            }
-        });
+                break;
+        }
     }
 }
