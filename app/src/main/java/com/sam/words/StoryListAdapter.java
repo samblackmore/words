484ef43209017f19e74b8898f5b97490b30a70c2
Sam Blackmore <samblackmore07@gmail.com>
2017-07-18 21:49:16 -0400

    adding cards

--- app/src/main/java/com/sam/words/StoryListAdapter.java
+++ app/src/main/java/com/sam/words/StoryListAdapter.java
@@ -1,5 +1,6 @@
 package com.sam.words;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
@@ -9,24 +10,25 @@ import android.widget.TextView;
  * Created by samhb on 2017-07-18.
  */
 
-public class StoryListAdapter extends RecyclerView.Adapter<StoryListAdapter.ViewHolder> {
+class StoryListAdapter extends RecyclerView.Adapter<StoryListAdapter.ViewHolder> {
     private String[] mDataset;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView mTextView;
+    static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public ViewHolder(TextView v) {
-            super(v);
-            mTextView = v;
+        CardView mCardView;
+        TextView mTextView;
+
+        ViewHolder(CardView cardView, TextView textView) {
+            super(cardView);
+            mCardView = cardView;
+            mTextView = textView;
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public StoryListAdapter(String[] myDataset) {
+    StoryListAdapter(String[] myDataset) {
         mDataset = myDataset;
     }
 
@@ -34,10 +36,12 @@ public class StoryListAdapter extends RecyclerView.Adapter<StoryListAdapter.View
     @Override
     public StoryListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        CardView v = (CardView) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.my_text_view, parent, false);
 
-        ViewHolder vh = new ViewHolder(v);
+        TextView textView = (TextView) v.findViewById(R.id.info_text);
+
+        ViewHolder vh = new ViewHolder(v, textView);
         return vh;
     }
 
