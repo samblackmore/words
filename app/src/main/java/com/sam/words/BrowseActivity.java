37c4b084528242d8e2f4ee05badb7b5673fda5c9
Sam Blackmore <samx_blackmore@mcafee.com>
2017-09-12 12:24:03 -0400

    adding sign out

--- app/src/main/java/com/sam/words/BrowseActivity.java
+++ app/src/main/java/com/sam/words/BrowseActivity.java
@@ -1,21 +1,26 @@
 package com.sam.words;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class BrowseActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,23 +46,46 @@ public class BrowseActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_tab, menu);
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser == null) {
+            MenuItem signOut = menu.findItem(R.id.action_sign_out);
+            signOut.setVisible(false);
+        }
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_sign_out:
+                mAuth.signOut();
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                if (currentUser == null)
+                    Toast.makeText(this, "Signed out", Toast.LENGTH_SHORT).show();
 
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-            return true;
+                FragmentManager fm = getSupportFragmentManager();
+
+                BrowseTabFragment frag = (BrowseTabFragment) fm.findFragmentById(R.id.container);
+
+                frag.signOut();
+
+                invalidateOptionsMenu();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/sam/words/BrowseTabFragment.java
+++ app/src/main/java/com/sam/words/BrowseTabFragment.java
@@ -1,20 +1,24 @@
 package com.sam.words;
 
 import android.app.DialogFragment;
-import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -31,11 +35,13 @@ import java.util.List;
  * A fragment representing one tab in the Browse Activity
  */
 
-public class BrowseTabFragment extends Fragment {
+public class BrowseTabFragment extends Fragment implements View.OnClickListener {
     // Fragment arguments
     private static final String ARG_TAB_TITLE = "TITLE";
 
     private FirebaseAuth mAuth;
+    private Button addStoryButton;
+    private ProgressBar progressBar;
     List<Story> mStories = new ArrayList<>();
 
     public BrowseTabFragment() {
@@ -57,11 +63,12 @@ public class BrowseTabFragment extends Fragment {
         if (getArguments().getInt(ARG_TAB_TITLE) == 3) {
 
             // Show the new story button
-            RelativeLayout addStoryContainer = (RelativeLayout) rootView.findViewById(R.id.add_story_container);
+            progressBar = (ProgressBar) rootView.findViewById(R.id.sign_in_progress);
+            FrameLayout addStoryContainer = (FrameLayout) rootView.findViewById(R.id.add_story_container);
             addStoryContainer.setVisibility(View.VISIBLE);
 
             // Set up button
-            Button addStoryButton = (Button) rootView.findViewById(R.id.add_story);
+            addStoryButton = (Button) rootView.findViewById(R.id.add_story);
             int color = getResources().getColor(R.color.colorAccent);
             int textColor = getResources().getColor(R.color.white);
             addStoryButton.getBackground().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
@@ -71,17 +78,10 @@ public class BrowseTabFragment extends Fragment {
             mAuth = FirebaseAuth.getInstance();
             FirebaseUser currentUser = mAuth.getCurrentUser();
 
-            if (currentUser == null) {
-                addStoryButton.setText(getResources().getString(R.string.sign_in));
-            } else {
-                addStoryButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        DialogFragment fragment = new NewStoryFragment();
-                        fragment.show(getActivity().getFragmentManager(), "newstory");
-                    }
-                });
-            }
+            if (currentUser == null)
+                signOut();
+
+            addStoryButton.setOnClickListener(this);
         }
 
 
@@ -130,5 +130,61 @@ public class BrowseTabFragment extends Fragment {
         });
 
         return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.add_story)
+            addNewStory();
+    }
+
+    public void signIn() {
+        addStoryButton.setText(getResources().getString(R.string.new_story));
+    }
+
+    public void signOut() {
+        addStoryButton.setText(getResources().getString(R.string.sign_in));
+    }
+
+    private void addNewStory() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+
+            // Already signed in
+            DialogFragment fragment = new NewStoryFragment();
+            fragment.show(getActivity().getFragmentManager(), "newstory");
+
+        } else {
+
+            // Change to sign in button
+            signOut();
+            progressBar.setVisibility(View.VISIBLE);
+            addStoryButton.setText("");
+            addStoryButton.setEnabled(false);
+
+            mAuth.signInAnonymously().addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    // Stop loading
+                    progressBar.setVisibility(View.GONE);
+                    addStoryButton.setEnabled(true);
+
+                    if (task.isSuccessful()) {
+                        signIn();
+
+                        // Add the sign out button to options menu
+                        BrowseActivity activity = (BrowseActivity) getActivity();
+                        activity.invalidateOptionsMenu();
+
+                        Toast.makeText(getActivity(), "Authentication success!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        signOut();
+                        Toast.makeText(getActivity(), "Authentication failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
