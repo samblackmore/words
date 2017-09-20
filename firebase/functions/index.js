46204b8e74e371462ebe9d00e7242409535f8627
Sam Blackmore <samx_blackmore@mcafee.com>
2017-09-20 10:18:44 -0400

    cleaning up firebase function

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,39 +1,21 @@
+'use strict';
+
 const functions = require('firebase-functions');
 
-// Listens for new posts added to /stories/:storyId/vote
-// and creates a vote end time at /stories/:storyId/voteEnds
+// Listen for vote finish becoming true then create next vote
 
 exports.finishUpdated = functions.database.ref('/stories/{storyId}/votes/{voteId}/finished')
 .onUpdate(event => {
 
-console.log("update detected on", event.params.storyId, event.params.voteId);
-return event.data.ref.parent.parent.parent.parent.parent.child('gotcha').push().set(true);
+  const voteId = parseInt(event.params.voteId);
+  const votesRef = event.data.ref.parent.parent;
+  const finished = event.data.val();
 
-})
-
-/*exports.gotPost = functions.database.ref('/stories/{storyId}/vote')
-    .onWrite(event => {
-
-        newPost = event.data.val()
-
-        console.log('got post', newPost, 'checking votes')
-
-        event.data.ref.parent.child('votes').once("value",
-        function(snapshot) {
-          if (snapshot.val() === null) {
-            return event.data.ref.parent.child('votes').push.set(
-            {
-            timeout: new Date().getTime() + 1000 * 60 * 5,
-            posts: [newPost]
-            }
-            )
-          }
-        },
-        function (errorObject) {
-          console.log("The read failed: " + errorObject.code);
-        });
-
-
-
-      //return event.data.ref.parent.child('voteEnds').set(new Date().getTime() + 1000 * 60 * 5);
-    });*/
+  if (finished) {
+    return votesRef.child(voteId + 1).set(
+    {
+      finished: false,
+      timeCreated: new Date().getTime()
+    });
+  }
+});
