f2dc0029f767153e2df3e945f9db539649f85ff3
Sam Blackmore <samx_blackmore@mcafee.com>
2017-09-20 17:01:17 -0400

    choosing winner

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -7,15 +7,40 @@ const functions = require('firebase-functions');
 exports.finishUpdated = functions.database.ref('/stories/{storyId}/votes/{voteId}/finished')
 .onUpdate(event => {
 
+  var promises = [];
+
   const voteId = parseInt(event.params.voteId);
-  const votesRef = event.data.ref.parent.parent;
+  const voteRef = event.data.ref.parent;
   const finished = event.data.val();
 
   if (finished) {
-    return votesRef.child(voteId + 1).set(
-    {
+
+    const chooseWinnerPromise = voteRef.child('posts').once('value').then(snapshot => {
+
+      var highestScore = -1;
+      var winningPostId = -1;
+
+      snapshot.forEach(function(childSnapshot) {
+        const postId = parseInt(childSnapshot.key)
+        const postScore = parseInt(childSnapshot.child('voteCount').val())
+
+        if (postScore > highestScore) {
+          highestScore = postScore
+          winningPostId = postId
+        }
+      })
+
+      return voteRef.child('winner').set(winningPostId)
+    })
+
+    const newVotePromise = voteRef.parent.child(voteId + 1).set({
       finished: false,
       timeCreated: new Date().getTime()
     });
+
+    promises.push(newVotePromise);
+    promises.push(chooseWinnerPromise);
+
+    return Promise.all(promises)
   }
 });
