ad73853ed3ed3588f8607674f9dfcbde5c765f4f
Sam Blackmore <samx_blackmore@mcafee.com>
2017-09-20 17:16:15 -0400

    temp

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -11,11 +11,14 @@ exports.finishUpdated = functions.database.ref('/stories/{storyId}/votes/{voteId
 
   const voteId = parseInt(event.params.voteId);
   const voteRef = event.data.ref.parent;
+  const storyRef = voteRef.parent.parent;
   const finished = event.data.val();
 
   if (finished) {
 
     const chooseWinnerPromise = voteRef.child('posts').once('value').then(snapshot => {
+
+      var winnerPromises = [];
 
       var highestScore = -1;
       var winningPostId = -1;
@@ -23,6 +26,7 @@ exports.finishUpdated = functions.database.ref('/stories/{storyId}/votes/{voteId
       snapshot.forEach(function(childSnapshot) {
         const postId = parseInt(childSnapshot.key)
         const postScore = parseInt(childSnapshot.child('voteCount').val())
+        const postMessage = childSnapshot.child('message').val()
 
         if (postScore > highestScore) {
           highestScore = postScore
@@ -30,7 +34,17 @@ exports.finishUpdated = functions.database.ref('/stories/{storyId}/votes/{voteId
         }
       })
 
-      return voteRef.child('winner').set(winningPostId)
+      const setWinnerPromise = voteRef.child('winner').set(winningPostId);
+      const addToStoryPromise = storyRef.child('chapters').limitToLast(1).once('value').then(chaptersSnapshot => {
+
+        var latestChapterId = -1
+
+        chaptersSnapshot.forEach(function(chapterSnapshot) {
+          latestChapterId = chapterSnapshot.key
+        })
+
+        return storyRef.child('chapters').child(latestChapterId).child('content').TRANSACTION
+      })
     })
 
     const newVotePromise = voteRef.parent.child(voteId + 1).set({
