7770e05416170fa20d4e46c4b1abfd8a76b1a931
Sam Blackmore <samblackmore07@gmail.com>
2018-02-24 00:01:06 -0500

    fix choose winner cloud function

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -18,97 +18,101 @@ exports.startRound = functions.database.ref('/posts/{storyId}/{roundId}')
 
 // Listen for vote finish becoming true then create next vote
 
-exports.finishUpdated = functions.database.ref('/poll/{storyId}/{chapterId}/{pollId}/finished')
-.onUpdate(event => {
+exports.finishUpdated = functions.database.ref('/poll/{storyId}/finished')
+.onWrite(event => {
 
-    const pollId = event.params.pollId;
-    const pollRef = event.data.ref.parent;
-    const chapterId = event.params.chapterId
-    const storyId = event.params.storyId;
-    const storyRef = event.data.ref.root.child('stories').child(storyId)
-    const postsRef = event.data.ref.root.child('posts')
     const finished = event.data.val();
+    const storyId = event.params.storyId;
+    const pollRef = event.data.ref.root.child('poll').child(storyId)
+    const storyRef = event.data.ref.root.child('stories').child(storyId)
+    const winnersRef = event.data.ref.root.child('winners').child(storyId)
+    const usersRef = event.data.ref.root.child('users')
 
     if (finished) {
 
         return pollRef.once('value').then(pollSnapshot => {
 
-            var highestScore = -1;
-            var winningPost;
+            const roundId = pollSnapshot.child('round').val()
+            const round = parseInt(roundId)
+            const postsRef = event.data.ref.root.child('posts').child(storyId).child(roundId)
 
-            pollSnapshot.child('posts').forEach(function(postSnapshot) {
+            return postsRef.once('value').then(postsSnapshot => {
 
-                const post = postSnapshot.val()
+                var highestScore = -1;
+                var winningPost;
+                var winningPostId;
 
-                if (post.voteCount > highestScore) {
-                    highestScore = post.voteCount
-                    winningPost = post
-                }
-            })
+                postsSnapshot.forEach(function(postSnapshot) {
 
-            return storyRef.once('value').then(storySnapshot => {
+                    const post = postSnapshot.val()
+                    const postId = postSnapshot.key
 
-                const round = parseInt(pollId)
-                const rounds = parseInt(storySnapshot.child('chapterSize').val())
+                    if (post.voteCount > highestScore) {
+                        highestScore = post.voteCount
+                        winningPost = post
+                        winningPostId = postId
+                    }
+                })
 
-                if (round < rounds) {
+                return storyRef.once('value').then(storySnapshot => {
 
-                    // Poll not over
-
-                    const newRound = round + 1
-
-                    // Push winning post
-                    const newPostRef = postsRef.child(storyId).child(chapterId).push();
-                    const newPostId = newPostRef.key
-                    newPostRef.set(winningPost)
-
-                    // Update winning user's posts
-                    postsRef.root.child('users').child(winningPost.userId).child('posts').child(newPostId).set(true)
-
-                    // Increment postCount on story
-                    storyRef.child('postCount').transaction(function (current_value) {
-                      return (current_value || 0) + 1;
-                    });
-
-                    // Start new round
-                    pollRef.parent.child(newRound).set({
-                        round: newRound,
-                        finished: false,
-                        timeCreated: new Date().getTime()
-                    })
-                } else {
-
-                    // Poll is over
-
-                    const newChapter = parseInt(chapterId) + 1
+                    const rounds = parseInt(storySnapshot.child('chapterSize').val())
                     const chapterLimit = parseInt(storySnapshot.child('chapterLimit').val())
 
-                    if (newChapter == chapterLimit) {
+                    if (round % rounds !== 0) {
 
-                        // Story finished
-                        storyRef.child('finished').set(true)
+                        console.log('adding winning post', winningPost)
 
-                    } else {
-                        // Add the next chapter
-                        storyRef.child('chapters').child(newChapter).set({chapter: newChapter})
+                        // Push winning post
+                        winnersRef.child(winningPostId).set(winningPost)
 
-                        // Increment chapterCount on story
-                        storyRef.child('chapterCount').transaction(function (current_value) {
+                        // Update winning user's posts
+                        usersRef.child(winningPost.userId).child('posts').child(winningPostId).set(true)
+
+                        // Increment postCount on story
+                        storyRef.child('postCount').transaction(function (current_value) {
                           return (current_value || 0) + 1;
                         });
 
-                        // Start first round of new chapter
-                        pollRef.root.child('poll').child(storyId).child(newChapter).child(0).set({
-                            round: 0,
-                            finished: false,
-                            timeCreated: new Date().getTime()
+                        // Start new round
+                        pollRef.update({
+                            round: round + 1
                         })
-                    }
+                    } else {
 
-                    // Assign winning title to story chapter
-                    storyRef.child('chapters').child(chapterId).child('title').set(winningPost.message)
-                }
+                        // Chapter is over
+
+                        const chapterId = round / rounds
+                        const newChapter = chapterId + 1
+
+                        console.log('chapter', chapterId, 'over')
+
+                        // Assign winning title to story chapter
+                        storyRef.child('chapters').child(chapterId).child('title').set(winningPost.message)
+
+                        if (newChapter == chapterLimit) {
+
+                            // Story finished
+                            storyRef.child('finished').set(true)
+                            pollRef.remove()
+
+                        } else {
+                            // Add the next chapter
+                            storyRef.child('chapters').child(newChapter).set({chapter: newChapter})
+
+                            // Increment chapterCount on story
+                            storyRef.child('chapterCount').transaction(function (current_value) {
+                              return (current_value || 0) + 1;
+                            });
+
+                            // Start new round
+                            pollRef.update({
+                                round: round + 1
+                            })
+                        }
+                    }
+                })
             })
         })
     }
-});
+})
